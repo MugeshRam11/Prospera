2e56eaf8ce9edeabcf54b634a9f8bbf1bb807360
MugeshRam11 <mugeshram396@gmail.com>
2026-06-17 12:23:15 +0530

    x

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -25,7 +25,6 @@ const Index = () => {
       <UaeOpportunityStickySection />
       <WhatWhyHowSection />
       <MarketIntelligence />
-      <TestimonialsSection />
       <StatsBar />
       <PartnerLogosMarquee />
       <FAQSection />
